578709b4ef6534b78ff0daa9cb02614a0b845926
Lal Verda Çakır <43840298+lalverdacakir@users.noreply.github.com>
2021-10-31 16:50:29 +0300

    comments added to encode_data function

--- encode.asm
+++ encode.asm
@@ -1,16 +1,13 @@
-segment .data
-intf db "%d", 10, 0
-
 
 segment .text
 global encode_data
 extern printf
 
 encode_data:
-    push ebx                        ;
-    push edi                        ; save the old di value
-    push esi                        ;
-    push ebp                        ; save the old base pointer value
+    push ebx                        ; save the old ebx
+    push edi                        ; save the old edi
+    push esi                        ; save the old esi
+    push ebp                        ; save the old ebp
 
     mov  ebp,esp                    ; base pointer <- stack pointer
     
@@ -24,117 +21,111 @@ encode_data:
     mov edi, [ebp + 12 + 4 + 4 + 4] ; edi<-nInputBytes
     shl edi, 3                      ; edi <- edi * 8 (8*nInoutBytes)
     sub edi, 4                      ; edi <- edi -4 (8*nInputBytes - 4)
+        
+
+    big_loop: cmp edi, -4               ; loop goes on edi-0
+        je end_loop_big                 ; if edi = -4 end the loop
+
+            mov edx, [ebp +8 +4 + 4 +4] ; edx <- argument 2 (inputBytes pointer)
+            mov edx, [edx]              ; edx<- input bytes content 
+        
+
+            mov ecx, edi                ; ecx <- edi  (to use in shift operation)
+            shr edx,cl                  ; edx <- edx >> ecx (shift the input bytes)
+            and edx, 15                 ; edx <- edx & 1111 (get the lowest 4 bit) current word
+            push dword edx              ; push the current word to stack this will become [w|p] (encoded data)
+        
+
+        
+        
+            xor eax,eax                 ; clear eax
+            xor ebx, ebx                ; clear ebx
+            xor ecx, ecx                ; clear ecx
+            xor edx,edx                 ; clear edx
+            
+
+        loop: cmp eax,16; compare eax 16 (traverse columns)
+            je end_loop ; if zflag = 0 jump end loop
+
+                
+            push dword 0                ; push 0 (will hold word * Hmr ith column = p[i])
+
+            mov edx, [ebp +8 +4+4+4]    ; edx <- argument 2 (inputBytes pointer) 
+            mov edx, [edx]              ; edx <- inputBytes content
+            mov ecx, edi                ; ecx <- edi  (to use in shift operation)
+            shr edx,cl                  ; edx <- edx >> ecx (shift the input bytes)
+            and edx,15                  ; edx <- edx & 1111 (get the lowest 4 bit) current word
+                                        ; line above (edx) word <- currenet word
+
+            mov  ecx,[ebp+28+4+4+4]     ; ecx <- Hmr pointer
+        
+            ;word[3] * Hmr[3][i] 
+            ;Not: indexes from lefthand side starting from 0
+
+            mov ebx, [ecx + eax + 48 ]  ; ebx<- Hmr[3][i]
+
+            and ebx, edx                ; ebx <- ebx & edx (Hmr[3][i] & word[3])
+            xor ebx, [ebp - 8]          ; ebx <- ebx xor p[i] 
+            mov [ebp-8], ebx            ; p[i] <- ebx
+            
+        
+            
+            mov ebx, [ecx + eax + 32]   ; ebx<- Hmr[2][i]
+
+            shr edx,1                   ; edx <- edx >> 1 (word <- current word >> 1)
+            and ebx, edx                ; ebx <- ebx & edx (Hmr[2][i] & word[2])
+
+            xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
+            mov [ebp-8], ebx            ; p[i] <- ebx
+
+            
+            mov ebx, [ecx + eax + 16]   ; ebx<- Hmr[1][i]
+            
+            shr edx,1                   ; edx <- edx >> 1 (word <- current word >> 1)
+            
+            and ebx, edx                ; ebx <- ebx & edx (Hmr[1][i] & word[1])
+
+            xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
+            mov [ebp-8], ebx            ; p[i] <- ebx
+
+            mov ebx, [ecx + eax + 0 ]   ; ebx<- Hmr[0][i]
+            shr edx,1                    ; edx <- edx >> 1 (word <- current word >> 1)
+            and ebx, edx                ; ebx <- ebx & edx (Hmr[1][i] & word[1])
+
+            xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
+            mov [ebp-8], ebx            ; p[i] <- ebx
+            
+
+            pop edx;                    ; pop p[i]
+            mov ebx, [ebp -4]           ; ebx <- current encoded
+            
+            shl ebx,1                   ; ebx <- ebx << 1 (shift current encoded to add p[i])
+            
+
+            or edx, ebx                 ; edx <- edx | ebx (current encoded | p[i])
+            mov [ebp - 4], edx          ; encoded <- current encoded
+
+            
+            add eax,4                   ; eax <- eax + 4 (increment)
+            
+        
+            jmp loop;                   ; jump begining of the loop
+
+        end_loop:
+            
+            sub edi, 4                  ; edi <- edi -4
+            
+
+            pop edx                     ; pop encoded
+            mov eax, [ebp + 16 +4+4+4]  ; eax <- encodedData pointer
+            mov byte[eax + esi],dl      ; encodedData[i] <- encoded (byte)
+            add esi,1                   ; esi <- esi + 1 (increment esi)
+            jmp big_loop                ; jump begining of the loop       
+            
+    end_loop_big:
     
-
-big_loop: cmp edi, -4; loop goes on edi-0
-    je end_loop_big; if edi = -4 end the loop
-
-        mov edx, [ebp +8 +4 + 4 +4] ; argument 2 (inputBytes pointer)
-        mov edx, [edx]              ; edx<- input bytes content 
-       
-
-        mov ecx, edi                ; ecx <- edi  (to use in shift operation)
-        shr edx,cl                  ; edx <- edx >> ecx (shift the input bytes)
-        and edx, 15                 ; <- edx <- edx & 1111 (get the lowest 4 bit) current word
-        push dword edx              ; push the current word to stack
-       
-
-    
-    
-        xor eax,eax                 ; clear eax
-        xor ebx, ebx                ; clear ebx
-        xor ecx, ecx                ; clear ecx
-        xor edx,edx                 ; clear edx
-        
-
-    loop: cmp eax,16; compare eax 16 (traverse columns)
-        je end_loop ; if zflag = 0 jump end loop
-
-        
-        
-        ;xor edx,edx; clear edx
-        push dword 0; push 0 (will hold word * current column)
-
-        mov edx, [ebp +8 +4+4+4]; edx <- inputBytes pointer
-        mov edx, [edx];edx <- content inputBytes
-       
-        mov ecx, edi; ecx <- edi for shift
-        shr edx,cl; edx <- edx >> cl
-       
-        and edx,15; edx <- edx &1111 (lowest 4 bits)
-        
-        mov  ecx,[ebp+28+4+4+4] ;ecx <- Hmr pointer
-
-       
-       
-        mov ebx, [ecx + eax + 48 ]; ebx<- Hmr[i][3]
-
-        and ebx, edx; Hmr[i][3] & word
-
-        xor ebx, [ebp - 8];
-        mov [ebp-8], ebx;
-        
-        
-        
-        
-        mov ebx, [ecx + eax + 32]; point edilen yer
-
-        shr edx,1;
-        and ebx, edx;
-
-        xor ebx, [ebp-8];
-        mov [ebp-8], ebx;
-
-        
-        mov ebx, [ecx + eax + 16]; point edilen yer
-        
-        shr edx,1;
-        
-        and ebx, edx;
-
-        xor ebx, [ebp-8];
-        mov [ebp-8], ebx;
-
-        mov ebx, [ecx + eax + 0 ]; point edilen yer
-        shr edx,1;
-        and ebx, edx;
-
-        xor ebx, [ebp-8];
-        mov [ebp-8], ebx;
-        
-
-        pop edx;
-        mov ebx, [ebp -4];
-        
-        shl ebx,1
-        
-
-        or edx, ebx;
-        mov [ebp - 4], edx;
-
-        
-        add eax,4;
-        
-    
-        jmp loop;
-
-    end_loop:
-        sub edi, 4;
-        
-
-        pop edx;
-        mov eax, [ebp + 16 +4+4+4];encodedData pointer
-        mov byte[eax + esi],0;
-        mov byte[eax + esi],dl;
-
-        add esp, 8;
-        add esi,1;
-        jmp big_loop;
-        
-end_loop_big:
-    
-    pop ebp ; fonksitondan çıkarken
-    pop esi;
-    pop edi;
-    pop ebx;
-    ret
+        pop ebp                         ; pop ebp
+        pop esi                         ; pop esi
+        pop edi                         ; pop edi
+        pop ebx                         ; pop ebx
+    ret                             ; ret from function
