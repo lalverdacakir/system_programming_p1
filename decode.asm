b7f79c0aa920a773678efd04713e1741a102d394
Lal Verda Çakır <43840298+lalverdacakir@users.noreply.github.com>
2021-11-02 13:55:19 +0300

    encode and decode

---
+++ decode.asm
@@ -0,0 +1,155 @@
+segment .data
+intf db "%d", 10, 0
+
+
+segment .text
+global decode_data
+extern printf
+
+decode_data:
+    push ebx                        ; save the old ebx
+    push edi                        ; save the old edi
+    push esi                        ; save the old esi
+    push ebp                        ; save the old ebp
+
+    mov  ebp,esp                    ; base pointer <- stack pointer
+    
+    xor edi,edi                     ; clear edi
+    xor esi,esi                     ; clear esi
+    xor eax, eax                    ; clear eax
+    xor ebx, ebx                    ; clear ebx
+    xor edx,edx                     ; clear edx
+    
+   
+    mov edi, 0;
+        push dword 0; syndrome
+		push dword 0; temp syndrome
+		push dword 0;
+		push dword 0; return value
+    data_loop: cmp edi,  [ebp + 12 + 4 + 4 + 4]               ; edi <- nEncodedBytes
+        je end_data_loop                                      ; if edi = -4 end the loop
+        
+		xor edx,edx;											; clear edx
+		mov eax, [ebp + 8 + 4 + 4 + 4]						  ; eax <- encodedbytes pointer
+		mov dl, byte[eax + edi]									; get current byte
+		
+		
+		mov [ebp-12], edx;										; push current
+		
+		shr edx, 4												;edx <- edx >> 4, edx <- data
+
+		mov ebx, edi											; ebx <- edi
+		shr ebx, 1												; ebx <- ebx >> 1 (ebx divided by 2) 
+		; 0th byte goes to 0 byte
+		; 1th byte goes to 0 byte
+		; 2th byte goes to 1 byte 
+		; 3th byte goes to 1 byte soo i >> 1
+
+		mov eax, [ebp + 16 + 4 + 4 + 4]							; eax <- decodedBytes
+		xor ecx, ecx											; clear ecx
+		mov cl, byte[eax + ebx]									; cl <-  decodedBytes[ebx]
+		shl ecx, 4												; ecx<- ecx << 4 make room for new decodedData
+		or ecx, edx												; ecx <- ecx | edx (make together data0 |data1)
+		mov byte[eax + ebx], cl									; write to decodedBytes 
+
+		
+
+		mov eax, 0; set eax 0
+		mov dword[ebp -4 ],0; set syndrome 0
+		
+		
+		traverse_column: cmp eax, 16					; compare eax 16
+						je end_traverse_column			;  end traverse column
+						mov edx, [ebp - 12]				; get value
+						
+						mov dword[ebp -8],0;			set temp sydrome 0
+						mov esi, 112					; set esi 112
+						traverse_row: cmp esi, -16		; compare esi -16
+						je end_traverse_row				; end row traversing
+						mov ebx, [ebp + 24 + 4 + 4 + 4] ; ebx <- H pointer
+						add ebx, esi			; ebx <- ebx + esi (H+i)
+						add ebx, eax			; ebx <- ebx + eax (H+i + j)
+						mov ecx, [ebx]			; ecx <- H[i][j]
+						
+						and ecx, edx			; ecx <- ecx and edx (multip)
+						xor ecx, [ebp-8]		; ecx <- ecx xor temp syndrome (addition)
+						mov [ebp-8], ecx		; temp syndrome <- ecx
+						shr edx,1				; edx <- edx >> 1, go for next bit of byte
+
+						sub esi, 16				; esi <- esi -16
+						
+						jmp traverse_row 		; next row
+						end_traverse_row:
+
+						
+							mov ebx, [ebp - 4]	; ebx <- sydrome
+							shl ebx,1			; ebx <- ebx << 1 make place for the next bit of sydrome	
+							or ebx, [ebp-8]		; ebx <- ebx or temp syndrome
+							mov [ebp - 4],ebx	; sydrome <- ebx
+							add eax, 4			; eax <- eax + 4
+							jmp traverse_column	; next column
+		end_traverse_column:
+		
+		; sydrome row
+		mov edx, [ebp-4]						; get syndrome
+		cmp edx, 0								; compare sydrome,0
+		je no_error								; if sydrome is 0 there is no error 
+		
+		mov ecx, 112							; ecx <- 112	
+		syndrome_traverse_row: cmp ecx,-16		;compare ecx, -16 see if it finished the wors
+		je no_error								; if rows finished there is no error found go to no error
+			mov edx, [ebp-4]					; get syndrome
+			xor eax,eax							;clear eax	
+			xor esi,esi							;clear esi
+			syndrome_traverse_column: cmp esi, 16; compare esi, 16 see if it finished columns
+				je end_syndrome_traverse_column		; if esi=16 jump end_sydrome_traverse column
+				shl eax,1							; eax << 1 make place for the row value
+				mov ebx, [ebp + 24 + 4 + 4 + 4]		; ebx <-  H pointer
+				add ebx, ecx						; ebx <- ebx +ecx H+i
+				add ebx, esi						; ebx <- ebx + esi H+i+j
+				or eax, [ebx]						; eax <- eax or H[i][j]
+				add esi,4							; esi <- esi +4
+				jmp syndrome_traverse_column		; next column
+			end_syndrome_traverse_column:	
+				
+				xor edx, eax						; edx <- edx xor eax (get difference)
+				cmp edx, 0							; if differance is none found error
+				je found_error						; if equal jumo found error
+
+				sub ecx,16							;ecx <- ecx -16
+				jmp syndrome_traverse_row			; next row
+		
+		
+		no_error: mov ebx, 0			; ebx<-0 if no error write 0 to error status
+		jmp cont_data_loop				; cont to data loop
+		
+		;if there is error, error status = 10000000 >> found_row_number
+		found_error: shr ecx, 4			; ecx >> 4 (ecx/16 = row number)
+		mov ebx, 128					;ebx <- 10000000
+		shr ebx, cl						; ebx<- ebx >> cl 
+		mov eax, [ebp -16]				; eax <- error_count		
+		add eax,1						; eax <- eax + 1 (error_count ++)	
+		mov [ebp -16],eax				; error_count <- eax
+		
+		cont_data_loop: 				;write the found error and go back to begining of the loop			
+
+		mov eax, [ebp + 32 + 12]		;eax <- errorStatus
+		mov byte[eax + edi], bl			;write error status to errorStatus[i]
+
+		
+		add edi, 1						;increment edi (i++)
+		
+		jmp data_loop					; go next encodedData value
+
+    end_data_loop:
+		pop eax							;return value
+		pop edx							; clear pushed values by popping
+		pop edx							; clear pushed values by popping			
+		pop edx							; clear pushed values by popping
+
+        pop ebp                         ; restore ebp value
+        pop esi                         ; restore esi value
+        pop edi                         ; restore edi value
+        pop ebx                         ; restore ebx value
+
+    ret                             ; ret from function
--- encode.asm
+++ encode.asm
@@ -1,3 +1,6 @@
+segment .data
+intf db "%d", 10, 0
+
 
 segment .text
 global encode_data
@@ -18,109 +21,132 @@ encode_data:
     xor edx,edx                     ; clear edx
     
    
-    mov edi, [ebp + 12 + 4 + 4 + 4] ; edi<-nInputBytes
-    shl edi, 3                      ; edi <- edi * 8 (8*nInoutBytes)
-    sub edi, 4                      ; edi <- edi -4 (8*nInputBytes - 4)
+    mov edi, 0;
         
 
-    big_loop: cmp edi, -4               ; loop goes on edi-0
-        je end_loop_big                 ; if edi = -4 end the loop
-
-            mov edx, [ebp +8 +4 + 4 +4] ; edx <- argument 2 (inputBytes pointer)
-            mov edx, [edx]              ; edx<- input bytes content 
-        
-
-            mov ecx, edi                ; ecx <- edi  (to use in shift operation)
-            shr edx,cl                  ; edx <- edx >> ecx (shift the input bytes)
-            and edx, 15                 ; edx <- edx & 1111 (get the lowest 4 bit) current word
-            push dword edx              ; push the current word to stack this will become [w|p] (encoded data)
-        
-
-        
-        
-            xor eax,eax                 ; clear eax
-            xor ebx, ebx                ; clear ebx
-            xor ecx, ecx                ; clear ecx
-            xor edx,edx                 ; clear edx
+    big_loop: cmp edi,  [ebp + 12 + 4 + 4 + 4]               ; loop goes on edi-0
+        je end_loop_big                                      ; if edi = -4 end the loop
+        mov esi,1                                            ; esi <- 1 (counter for datas in byte first and second)
+        byte_tara: cmp esi, -1                               ; compare esi with 1
+            je end_byte_tara                                 ; end byte tara
+                mov edx, [ebp +8 +4 + 4 +4]  ; edx <- argument 2 (inputBytes pointer)
+                
+                add edx, edi                ; edx <- inputBytes + byte counter
+               
+                xor ebx,ebx                 ; clear ebx        
+                mov bl, byte[edx]           ; bl <- data in inputBytes + bytecounter
+                 
+                mov ecx, esi                ; ecx <- esi  (to use in shift operation)
+                shl ecx, 2                  ; ecx = ecx*4         
+                shr ebx,cl                  ; ebx <- ebx >> ecx (shift the input bytes)
+                and ebx, 15                 ; ebx <- ebx & 1111 (get the lowest 4 bit) current word
+                push dword ebx              ; push the current word to stack this will become [w|p] (encoded data)
+                
             
+                xor eax,eax                 ; clear eax
+                xor ebx, ebx                ; clear ebx
+                xor ecx, ecx                ; clear ecx
+                xor edx,edx                 ; clear edx
+                
 
-        loop: cmp eax,16; compare eax 16 (traverse columns)
-            je end_loop ; if zflag = 0 jump end loop
+            loop: cmp eax,16                ; compare eax 16 (traverse columns)
+                je end_loop                 ; if zflag = 0 jump end loop
+
+                    
+                push dword 0                ; push 0 (will hold word * Hmr ith column = p[i])
+                
+
+                mov edx, [ebp +8 +4 + 4 +4] ; edx <- argument 2 (inputBytes pointer)
+                ;mov edx, [edx]             ; edx<- input bytes content 
+                add edx, edi                ; edx <- inputBytes + byte counter
+                xor ebx,ebx                 ; clear ebx        
+                mov bl, byte[edx]           ; bl <- data in inputBytes + bytecounter
+                
+                mov ecx, esi                ;ecx <- esi  (to use in shift operation)
+                shl ecx, 2                  ; mul 4          
+                shr ebx,cl                  ; edx <- edx >> ecx (shift the input bytes)
+                and ebx, 15                 ; edx <- edx & 1111 (get the lowest 4 bit) current word
+
+                mov edx,ebx
 
                 
-            push dword 0                ; push 0 (will hold word * Hmr ith column = p[i])
 
-            mov edx, [ebp +8 +4+4+4]    ; edx <- argument 2 (inputBytes pointer) 
-            mov edx, [edx]              ; edx <- inputBytes content
-            mov ecx, edi                ; ecx <- edi  (to use in shift operation)
-            shr edx,cl                  ; edx <- edx >> ecx (shift the input bytes)
-            and edx,15                  ; edx <- edx & 1111 (get the lowest 4 bit) current word
-                                        ; line above (edx) word <- currenet word
-
-            mov  ecx,[ebp+28+4+4+4]     ; ecx <- Hmr pointer
-        
-            ;word[3] * Hmr[3][i] 
-            ;Not: indexes from lefthand side starting from 0
-
-            mov ebx, [ecx + eax + 48 ]  ; ebx<- Hmr[3][i]
-
-            and ebx, edx                ; ebx <- ebx & edx (Hmr[3][i] & word[3])
-            xor ebx, [ebp - 8]          ; ebx <- ebx xor p[i] 
-            mov [ebp-8], ebx            ; p[i] <- ebx
+                mov  ecx,[ebp+28+4+4+4]     ; ecx <- Hmr pointer
             
-        
-            
-            mov ebx, [ecx + eax + 32]   ; ebx<- Hmr[2][i]
+                ;word[3] * Hmr[3][i] 
+                ;Not: indexes from lefthand side starting from 0
 
-            shr edx,1                   ; edx <- edx >> 1 (word <- current word >> 1)
-            and ebx, edx                ; ebx <- ebx & edx (Hmr[2][i] & word[2])
+                mov ebx, [ecx + eax + 48 ]  ; ebx<- Hmr[3][i]
 
-            xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
-            mov [ebp-8], ebx            ; p[i] <- ebx
+                and ebx, edx                ; ebx <- ebx & edx (Hmr[3][i] & word[3])
+                xor ebx, [ebp - 8]          ; ebx <- ebx xor p[i] 
+                mov [ebp-8], ebx            ; p[i] <- ebx
+                
+            
+                
+                mov ebx, [ecx + eax + 32]   ; ebx<- Hmr[2][i]
 
-            
-            mov ebx, [ecx + eax + 16]   ; ebx<- Hmr[1][i]
-            
-            shr edx,1                   ; edx <- edx >> 1 (word <- current word >> 1)
-            
-            and ebx, edx                ; ebx <- ebx & edx (Hmr[1][i] & word[1])
+                shr edx,1                   ; edx <- edx >> 1 (word <- current word >> 1)
+                and ebx, edx                ; ebx <- ebx & edx (Hmr[2][i] & word[2])
 
-            xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
-            mov [ebp-8], ebx            ; p[i] <- ebx
+                xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
+                mov [ebp-8], ebx            ; p[i] <- ebx
 
-            mov ebx, [ecx + eax + 0 ]   ; ebx<- Hmr[0][i]
-            shr edx,1                    ; edx <- edx >> 1 (word <- current word >> 1)
-            and ebx, edx                ; ebx <- ebx & edx (Hmr[1][i] & word[1])
+                
+                mov ebx, [ecx + eax + 16]   ; ebx<- Hmr[1][i]
+                
+                shr edx,1                   ; edx <- edx >> 1 (word <- current word >> 1)
+                
+                and ebx, edx                ; ebx <- ebx & edx (Hmr[1][i] & word[1])
 
-            xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
-            mov [ebp-8], ebx            ; p[i] <- ebx
-            
+                xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
+                mov [ebp-8], ebx            ; p[i] <- ebx
 
-            pop edx;                    ; pop p[i]
-            mov ebx, [ebp -4]           ; ebx <- current encoded
-            
-            shl ebx,1                   ; ebx <- ebx << 1 (shift current encoded to add p[i])
-            
+                mov ebx, [ecx + eax + 0 ]   ; ebx<- Hmr[0][i]
+                shr edx,1                    ; edx <- edx >> 1 (word <- current word >> 1)
+                and ebx, edx                ; ebx <- ebx & edx (Hmr[1][i] & word[1])
 
-            or edx, ebx                 ; edx <- edx | ebx (current encoded | p[i])
-            mov [ebp - 4], edx          ; encoded <- current encoded
+                xor ebx, [ebp-8]            ; ebx <- ebx xor p[i]
+                mov [ebp-8], ebx            ; p[i] <- ebx
+                
 
-            
-            add eax,4                   ; eax <- eax + 4 (increment)
-            
-        
-            jmp loop;                   ; jump begining of the loop
+                pop edx;                    ; pop p[i]
+                mov ebx, [ebp -4]           ; ebx <- current encoded
+                
+                shl ebx,1                   ; ebx <- ebx << 1 (shift current encoded to add p[i])
+                
 
-        end_loop:
+                or edx, ebx                 ; edx <- edx | ebx (current encoded | p[i])
+                mov [ebp - 4], edx          ; encoded <- current encoded
+                
+                
+                add eax,4                   ; eax <- eax + 4 (increment)
+                
             
-            sub edi, 4                  ; edi <- edi -4
-            
+                jmp loop;                   ; jump begining of the loop
 
-            pop edx                     ; pop encoded
-            mov eax, [ebp + 16 +4+4+4]  ; eax <- encodedData pointer
-            mov byte[eax + esi],dl      ; encodedData[i] <- encoded (byte)
-            add esi,1                   ; esi <- esi + 1 (increment esi)
-            jmp big_loop                ; jump begining of the loop       
+            end_loop:
+                
+                
+                
+
+                pop edx                     ; pop encoded
+                mov eax, [ebp + 16 +4+4+4]  ; eax <- encodedData pointer
+                mov ebx, edi                ; ebx <- edi (byte counter)
+                shl ebx,1                   ; byte counter *2
+                add ebx,1                   ; byte counter * 2 +1      
+                sub ebx, esi                ; byte counter *2 +1 - esi
+                add eax, ebx                ; encodedData + byte counter *2 +1 - esi (the place I will write the result)         
+                
+    
+                mov byte[eax],dl            ; encodedData[i] <- encoded (byte)
+                sub esi,1                   ; esi <- esi + 1 (increment esi)
+                jmp byte_tara               ; go to next 4 bit in byte
+                 
+        end_byte_tara:
+                
+                add edi,1                   ; increment edi (byte counter)
+                jmp big_loop                ; jump begining of the loop     
             
     end_loop_big:
     
